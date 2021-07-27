6b1a1bd098a570188f0ec735ea9f906008a1c08a
sly <sly@qq.com>
2021-07-27 15:37:50 +0800

    hotfix commit

--- src/main/java/com/sly/App.java
+++ src/main/java/com/sly/App.java
@@ -10,6 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("sdf");
-        System.out.println("sdfadsa");
+        System.out.println("sdhotfix");
     }
 }
